de40aa043eaf400eddf908a0921b84be713cce0d
Faraz Fallah <farazfallah0@gmail.com>
2024-11-25 12:07:09 +0330

    Some changes in Order Model

--- orders/models.py
+++ orders/models.py
@@ -4,19 +4,25 @@ import uuid
 
 
 class Order(models.Model):
+    STATUS_CHOICES = [
+        ('registered', 'Registered'),
+        ('pending', 'Pending'),
+        ('confirmed', 'Awaiting Confirmation'),
+        ('shipped', 'Shipped'),
+        ('completed', 'Completed'),
+        ('canceled', 'Canceled'),
+    ]
+
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     customer = models.ForeignKey('customers.Customer', on_delete=models.CASCADE, related_name='orders')
+    shipping_address = models.OneToOneField('customers.Address', on_delete=models.SET_NULL, null=True, blank=True, related_name='order')
     total_price = models.DecimalField(max_digits=10, decimal_places=2)
-    status = models.CharField(max_length=50, choices=[
-        ('pending', 'Pending'),
-        ('completed', 'Completed'),
-        ('canceled', 'Canceled')
-    ])
+    status = models.CharField(max_length=50, choices=STATUS_CHOICES, default='registered')
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
-        return f"Order {self.id} by {self.customer}"
+        return f"Order {self.id} by {self.customer} ({self.get_status_display()})"
 
 
 class OrderItem(models.Model):
