abd29d89c7dbd47bb79315010feb84e9290fca4d
Faraz Fallah <farazfallah0@gmail.com>
2024-11-28 23:06:12 +0330

    Product Attribute system

--- product/admin.py
+++ product/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
 from .models import Category, Attribute, Product, ProductComment, ProductAttribute
 
+class ProductAttributeInline(admin.TabularInline):
+    model = ProductAttribute
+    extra = 1
+
 class CategoryAdmin(admin.ModelAdmin):
     list_display = ('name', 'description', 'parent_category')
     search_fields = ('name',)
@@ -15,18 +19,14 @@ class ProductAdmin(admin.ModelAdmin):
     search_fields = ('name', 'category__name')
     list_filter = ('category',)
     list_editable = ('price', 'stock_quantity')
+    inlines = [ProductAttributeInline]
 
 class ProductCommentAdmin(admin.ModelAdmin):
     list_display = ('product', 'customer', 'rating', 'status')
     list_filter = ('status',)
     search_fields = ('product__name', 'customer__username')
 
-class ProductAttributeAdmin(admin.ModelAdmin):
-    list_display = ('product', 'attribute', 'value')
-    search_fields = ('product__name', 'attribute__name')
-
 admin.site.register(Category, CategoryAdmin)
 admin.site.register(Attribute, AttributeAdmin)
 admin.site.register(Product, ProductAdmin)
 admin.site.register(ProductComment, ProductCommentAdmin)
-admin.site.register(ProductAttribute, ProductAttributeAdmin)
